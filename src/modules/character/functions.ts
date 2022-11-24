42fc6030397061e84fce4da47c05d8f8d1e60afa
Kevin Haube <khaube@fearless.tech>
2022-11-23 22:44:45 -0500

    Updates to the object model

--- src/modules/character/functions.ts
+++ src/modules/character/functions.ts
@@ -1,20 +1,48 @@
 import {EmptyParamError, InvalidParamError, LongNameError} from "./errors";
 import { Character, CharacterClass, CharacterRace } from "./models";
 
-// type Middleware<T> = (arg: T) => void;
-// function validate<T>(value: T, ...mw: Middleware<T>[]) {
-//   mw.forEach((func) => func(value));
-// }
-// function validateMany(values: any[], ...mw: Middleware<any>[]) {
-//   values.forEach((value) => validate(value, ...mw));
-// }
+const CHARACTER_BUILDER = "CharacterBuilder"
+
+type Middleware<T> = (arg: T, context: string) => void;
+type MiddlewareConfig = Partial<Record<keyof Character, Middleware<any>[]>>
+
+function nameLength(name: string, context: string) {
+  if (name.length > 48) {
+    throw new LongNameError(`${context}: name cannot exceed 48 characters - given ${name.length}`)
+  }
+}
+
+function nameEmpty(name: string, context: string) {
+  if (name.trim() === "") { 
+    throw new EmptyParamError(`${context}: name cannot be empty`, "name") 
+  } 
+}
+
+function enforceRaceType(race: string, context: string) {
+  if (!Object.values(CharacterRace).includes(race as CharacterRace)) {
+    throw new InvalidParamError(`${context}: ${race} is not a valid CharacterRace`, "race");
+  }
+}
+
+function enforceClassType(cClass: string, context: string) {
+  if (!Object.values(CharacterClass).includes(cClass as CharacterClass)) {
+    throw new InvalidParamError(`${context}: ${cClass} is not a valid CharacterClass`, "class");
+  }
+}
+
 /** Creates an empty Character object, and provides public methods to build the character
  *  with input validation and friendly errors.
  */
 export class CharacterBuilder {
+  private readonly validators: MiddlewareConfig;
   private readonly character: Partial<Character>;
   /** Initialize undefined level 1 character */
   constructor() {
+    this.validators = {
+      name: [nameLength, nameEmpty],
+      race: [enforceRaceType],
+      cClass: [enforceClassType]
+    }
     this.character = {
       name: undefined,
       race: undefined,
@@ -30,25 +58,37 @@ export class CharacterBuilder {
   }
   /** Set the character's name with empty string and length validation */
   setName(name: string) {
-    if (name.trim() === "") { 
-      throw new EmptyParamError(`${this.constructor.name}: name cannot be empty`, "name") 
-    } else if (name.length > 48) {
-      throw new LongNameError(`${this.constructor.name}: name cannot exceed 48 characters - given ${name.length}`)
+    if (this.validate("name", name)) {
+      this.character.name = name;
+    } else {
+      console.error("Character name could not be updated")
     }
-    this.character.name = name;
   }
   /** Set the character's race with runtime type check for {@link CharacterRace} */
   setRace(race: CharacterRace) {
-    if (!Object.values(CharacterRace).includes(race)) {
-      throw new InvalidParamError(`${this.constructor.name}: ${race} is not a valid CharacterRace`, "race");
+    if (this.validate("race", race)) {
+      this.character.race = race;
+    } else {
+      console.error("Character race could not be updated")
     }
-    this.character.race = race;
   }
   /** Set the character's class with runtime type check for {@link CharacterClass} */
   setClass(cClass: CharacterClass) {
-    if (!Object.values(CharacterClass).includes(cClass)) {
-      throw new InvalidParamError(`${this.constructor.name}: ${cClass} is not a valid CharacterClass`, "class");
+    if (this.validate("cClass", cClass)) {
+      this.character.cClass = cClass;
+    } else {
+      console.error("Character class could not be updated")
     }
-    this.character.cClass = cClass;
+  }
+
+  private validate(attr: keyof Character, ...args: any[]) {
+    const validator = this.validators?.[attr]
+    if (!!validator) {
+      validator.forEach((func) => args.forEach((arg) => func(arg, CHARACTER_BUILDER)));
+      return true;
+    } else {
+      console.error(`Validators for ${attr} not provided for CharacterBuilder.validate`)
+      return false;
+    }
   }
 }
