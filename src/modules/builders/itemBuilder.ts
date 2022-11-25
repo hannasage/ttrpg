de491ede9bbdef45404d3bc838d158941bfcd7f8
Kevin Haube <khaube@fearless.tech>
2022-11-24 21:11:17 -0500

    Adding weapon builder

--- src/modules/builders/itemBuilder.ts
+++ src/modules/builders/itemBuilder.ts
@@ -1,15 +1,29 @@
 import {BuilderWithMiddleware, SetupBuilder} from "../../utils/builderWithMiddleware";
-import {Item} from "../core/models/itemModels";
+import {Dice} from "../core/enums/dice";
+import {Item, Weapon, WeaponActions, WeaponRating} from "../core/models/itemModels";
 
 /** The highest-order parent of any item classes */
-class ItemBuilder extends BuilderWithMiddleware<Item> {
-    constructor({ validators, baseline }: SetupBuilder<Item>) {
-        super({ validators, baseline })
-    }
+class ItemBuilder<T extends Item> extends BuilderWithMiddleware<T> {
+    constructor(setup?: SetupBuilder<T>) { super(setup) }
     /** Set weapon name with runtime validation */
     setName(name: string) { this.setAttr("name", name) }
 }
 //TODO: Build an item factory
-export class WeaponBuilder extends ItemBuilder {}
-export class ArmorBuilder extends ItemBuilder {}
-export class MaterialBuilder extends ItemBuilder {}
+export class WeaponBuilder extends ItemBuilder<Weapon> {
+    constructor(setup?: SetupBuilder<Weapon>) { super(setup) }
+    // inherits setName from ItemBuilder
+    setRange(range: number) { this.setAttr("range", range) }
+    /** Set actions a weapon can perform on a combat turn */
+    setActions(actions: WeaponActions) { this.setAttr("actions", actions) }
+    /** Set the damage dice for a weapon's attack
+     *  @example
+     *  this.setDamage(2, "d12") // value: 2d12
+     */
+    setDamage(count: number, die: Dice) { this.setAttr("damage", `${count}${die}`) }
+    /** Set the rating of the weapon */
+    setRating(rating: WeaponRating) { this.setAttr("rating", rating) }
+    /** Set an optional attack limit */
+    setLimit(limit: number) { this.setAttr("limit", limit) }
+}
+// export class ArmourBuilder extends ItemBuilder<Armour> {}
+// export class MaterialBuilder extends ItemBuilder<Material> {}
--- src/modules/core/models/itemModels.ts
+++ src/modules/core/models/itemModels.ts
@@ -1,3 +1,31 @@
+/** Core attributes for an item */
 export interface Item {
     name: string;
+}
+/** Core action */
+export interface Action {
+    name: string;
+    description: string;
+}
+/** Core actions a weapon can take */
+export interface WeaponActions {
+    action: Action;
+    bonusAction: Action[];
+    buffs: Action[];
+}
+/** Core weapon rating values */
+export enum WeaponRating {
+    POOR = "poor",
+    FINE = "fine",
+    GOOD = "good",
+    GREAT = "great",
+    PRISTINE = "pristine"
+}
+/** Core attributes for a weapon */
+export interface Weapon extends Item {
+    range: number;
+    actions: WeaponActions;
+    damage: number;
+    rating: WeaponRating;
+    limit?: number;
 }
