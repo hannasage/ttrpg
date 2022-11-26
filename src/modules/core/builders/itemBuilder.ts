fa09c3cd205c01219246de3de2cd131e4eedba32
Kevin Haube <khaube@fearless.tech>
2022-11-25 19:17:11 -0500

    Move attributes to parent class

--- src/modules/core/builders/itemBuilder.ts
+++ src/modules/core/builders/itemBuilder.ts
@@ -7,21 +7,21 @@ class ItemBuilder<T extends Item> extends BuilderWithMiddleware<T> {
     constructor(setup?: SetupBuilder<T>) { super(setup) }
     /** Set weapon name with runtime validation */
     setName(name: string) { this.setAttr("name", name) }
+    /** Set actions a weapon can perform on a combat turn */
+    setActions(actions: ItemActions) { this.setAttr("actions", actions) }
+    /** Set the rating of the weapon */
+    setRating(rating: ItemRating) { this.setAttr("rating", rating) }
 }
 //TODO: Build an item factory
 export class WeaponBuilder extends ItemBuilder<WeaponItem> {
     constructor(setup?: SetupBuilder<WeaponItem>) { super(setup) }
     // inherits setName from ItemBuilder
     setRange(range: number) { this.setAttr("range", range) }
-    /** Set actions a weapon can perform on a combat turn */
-    setActions(actions: ItemActions) { this.setAttr("actions", actions) }
     /** Set the damage dice for a weapon's attack
      *  @example
      *  this.setDamage(2, "d12") // value: 2d12
      */
     setDamage(count: number, die: Dice) { this.setAttr("damage", `${count}${die}`) }
-    /** Set the rating of the weapon */
-    setRating(rating: ItemRating) { this.setAttr("rating", rating) }
     /** Set an optional attack limit */
     setLimit(limit: number) { this.setAttr("limit", limit) }
 }
